1d1f33bf060613681581226441694f97425dfa5d
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-05-17 22:24:57 +0200

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
     <Layout>
-        <SEO title="404: Not found" />
+        <SEO title="Not found" />
         
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div id="not-found">
+            <div>
+                <h1>404</h1>
+                <p>
+                    There's nothing here. <Link to="/">Go Home?</Link>
+                </p>
+            </div>
+        </div>
     </Layout>
 )
 
