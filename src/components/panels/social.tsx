b5eadfbe3dfd43480bd7bbec405867c683ac5471
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-05-22 22:56:24 +0200

    add linkedin

--- src/components/panels/social.tsx
+++ src/components/panels/social.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+import linkedinIcon from '../../images/icons/linkedin.svg'
 import angellistIcon from '../../images/icons/angellist.svg'
 import githubIcon from '../../images/icons/github.svg'
 
@@ -12,6 +13,11 @@ const PanelSocial = () => {
                     <path className="stroke" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M16.7 20.2c76.5 4.4 153.6-9.7 229.8-4.1 5.4.4 12.4 2.1 11.7 5.6-67.3 1.7-134.5 5.5-201.2 11.5l87.7-.9c35.2-.4 70.8-.7 104.9 4.6"></path>
                 </svg>
             </h1>
+
+            <p>
+                <img className="icon-social" src={linkedinIcon} alt="LinkedIn" />
+                LinkedIn – <a href="https://www.linkedin.com/in/raphaelschuenzel/" target="_blank">Raphael Schünzel</a>
+            </p>
 
             <p>
                 <img className="icon-social" src={githubIcon} alt="GitHub" />
