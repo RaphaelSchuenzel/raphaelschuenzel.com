cb000a334e068c99c25c950e2eb04b413151d63a
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-05-23 00:30:45 +0200

    tsx svg naming fixes

--- src/components/panels/social.tsx
+++ src/components/panels/social.tsx
@@ -10,7 +10,7 @@ const PanelSocial = () => {
             <h1 className="panel-head">
                 Find me on...
                 <svg className="underline" width="100%" height="50" aria-hidden="true">
-                    <path className="stroke" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M16.7 20.2c76.5 4.4 153.6-9.7 229.8-4.1 5.4.4 12.4 2.1 11.7 5.6-67.3 1.7-134.5 5.5-201.2 11.5l87.7-.9c35.2-.4 70.8-.7 104.9 4.6"></path>
+                    <path className="stroke" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M16.7 20.2c76.5 4.4 153.6-9.7 229.8-4.1 5.4.4 12.4 2.1 11.7 5.6-67.3 1.7-134.5 5.5-201.2 11.5l87.7-.9c35.2-.4 70.8-.7 104.9 4.6"></path>
                 </svg>
             </h1>
 
